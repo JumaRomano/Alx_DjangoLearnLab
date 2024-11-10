ba6a72df196355d03053d3a903dd631a975e4a37
Romano Victor <romanojuma15@gmail.com>
2024-11-10 21:32:35 +0300

    whue

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 from django.views.generic import DetailView
 from .models import Library
 
-# Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
-    def get_queryset(self):
-        # Ensure that we're fetching libraries from the database
-        return Library.objects.all()
