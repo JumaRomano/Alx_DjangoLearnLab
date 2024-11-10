0ef9846c7b61ed73a14420c0052a16b99464ed98
Romano Victor <romanojuma15@gmail.com>
2024-11-10 22:23:00 +0300

    changed

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('LogoutView.as_view(template_name=", "LoginView.as_view(template_name='),
 ]
+LogoutView.as_view(template_name=", "LoginView.as_view(template_name=
