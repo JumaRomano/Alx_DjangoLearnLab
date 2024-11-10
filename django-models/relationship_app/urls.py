8117cee73c8ebc326def3e1d138970217bbe1a06
Romano Victor <romanojuma15@gmail.com>
2024-11-10 22:30:44 +0300

    changes done

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -27,3 +27,17 @@ urlpatterns = [
     path('LogoutView.as_view(template_name=", "LoginView.as_view(template_name='),
 ]
 LogoutView.as_view(template_name=", "LoginView.as_view(template_name=
+                                                       
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -48,3 +48,30 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+# Check if user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Check if user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Check if user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
