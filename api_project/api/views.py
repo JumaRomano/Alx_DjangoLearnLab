ef37d0580fb04955fab641cdd7104b387095815f
Romano Victor <romanojuma15@gmail.com>
2024-11-24 01:07:55 +0300

    DONE

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,7 +5,7 @@ from rest_framework .generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
-
+viewsets.ModelViewSet
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
