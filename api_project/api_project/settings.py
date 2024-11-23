9d39427b6f0448011cde4f88d28c5b22f4ed79c8
Romano Victor <romanojuma15@gmail.com>
2024-11-24 01:39:58 +0300

    done

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -128,4 +128,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # Use token authentication (or other methods if needed)
+    ],
 }
+
