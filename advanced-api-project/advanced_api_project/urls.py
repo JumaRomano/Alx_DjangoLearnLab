ae6696354460f0ca30d3c3816cc0783ba0caf7f8
Romano Victor <romanojuma15@gmail.com>
2024-12-02 00:15:35 +0300

    done

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -25,3 +25,8 @@ from django.urls import path, include
 urlpatterns = [
     path('api/', include('api.urls')),
 ]
+from django.urls import path, include
+
+urlpatterns = [
+    path('api/', include('api.urls')),
+]
